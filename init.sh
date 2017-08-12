6ddc0e86a87f0de5aaea807d18fb933be47216eb
svlord <losev@frtk.ru>
2017-08-12 19:45:10 +0300

    add folders and configuration

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx  /etc/nginx/sites-enabled/test.conf
+sudo rm -r /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
