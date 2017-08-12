55f3c78a302b8a70e1cb299f6d006458f8159bd6
svlord <losev@frtk.ru>
2017-08-12 18:53:42 +0300

    add folders and configuration

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/stepic/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/stepic/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
